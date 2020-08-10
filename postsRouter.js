4c25e7baf3d83c3317f2b98415ad74b672453632
Vish Singh <vishalicious213@gmail.com>
2020-08-09 18:26:31 -0700

    DELETE /api/posts/:id done

--- postsRouter.js
+++ postsRouter.js
@@ -55,4 +55,24 @@ postsRouter.get('/:id/comments', (req, res) => {
         })
 });
 
+postsRouter.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    db.remove(id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist",
+                })
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: 'The post could not be removed',
+            })
+        })
+});
+
 module.exports = postsRouter;
