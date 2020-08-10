1bdd488400155659ed3186276895824e739609ca
Vish Singh <vishalicious213@gmail.com>
2020-08-09 18:13:31 -0700

    GET /api/posts/:id/comments done

--- postsRouter.js
+++ postsRouter.js
@@ -35,4 +35,24 @@ postsRouter.get('/:id', (req, res) => {
         })
 });
 
+postsRouter.get('/:id/comments', (req, res) => {
+    const postId = req.params.id;
+    db.findPostComments(postId)
+        .then(comments => {
+            if (postId) {
+                res.status(200).json(comments)
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist",
+                })
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: 'The comments information could not be retrieved',
+            })
+        })
+});
+
 module.exports = postsRouter;
