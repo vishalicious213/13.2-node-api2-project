5332ca665580a3ea0365274e9ac9734619919b68
Vish Singh <vishalicious213@gmail.com>
2020-08-09 18:00:57 -0700

    GET /api/posts/:id done

--- postsRouter.js
+++ postsRouter.js
@@ -15,4 +15,24 @@ postsRouter.get('/', (req, res) => {
         })
 });
 
+postsRouter.get('/:id', (req, res) => {
+    const id = req.params.id;
+    db.findById(id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist",
+                })
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: 'The post information could not be retrieved',
+            })
+        })
+});
+
 module.exports = postsRouter;
